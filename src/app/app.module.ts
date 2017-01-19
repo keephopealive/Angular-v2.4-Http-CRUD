9daa4bbcb4eef73479db10962e57e0e4520cedae
Speros Misirlakis <smisirlakis@gmail.com>
2017-01-19 16:36:02 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { HttpService } from './http.service';
+import { WidgetService } from './widget.service';
+
 import { AppComponent } from './app.component';
+import { WidgetComponent } from './widget/widget.component';
+import { WidgetListComponent } from './widget-list/widget-list.component';
+import { WidgetNewComponent } from './widget-new/widget-new.component';
+import { WidgetEditComponent } from './widget-edit/widget-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetComponent,
+    WidgetListComponent,
+    WidgetNewComponent,
+    WidgetEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [WidgetService, HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
